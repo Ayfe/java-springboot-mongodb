50642a377d0b5cd1122a13d07383a337b0c5be94
up <up@192.168.150.14>
2019-01-09 14:38:57 +0100

    adding find methods

--- src/main/java/com/groupeup/poc/web/AffiliateController.java
+++ src/main/java/com/groupeup/poc/web/AffiliateController.java
@@ -70,10 +70,46 @@ public class AffiliateController {
         return resp;
     }
     
+    @RequestMapping(method = RequestMethod.GET, value="/user/getAffiliates")
+    public AffiliateResponse getAffiliatesUser() {
+        List<Document> result = new ArrayList<>();
+        mongoClientUser.getDatabase("db-ldl-fr").getCollection("affiliates").find().into(result);
+
+        AffiliateResponse resp = new AffiliateResponse();
+        resp.response = result;
+
+        return resp;
+    }
+    
+    
+    @RequestMapping(method = RequestMethod.GET, value="/unknown/getAffiliates")
+    public AffiliateResponse getAffiliatesUnknown() {
+        List<Document> result = new ArrayList<>();
+        mongoClientUnknown.getDatabase("db-ldl-fr").getCollection("affiliates").find().into(result);
+
+        AffiliateResponse resp = new AffiliateResponse();
+        resp.response = result;
+
+        return resp;
+    }
+    
+    
     @RequestMapping(method = RequestMethod.GET, value="/erp/affiliateByVat/{vat}")
     public AffiliateResponse getAffiliateErpByVat(@PathVariable String vat) {
         List<Document> result = new ArrayList<>();
         mongoClientERP.getDatabase("db-ldl-fr").getCollection("affiliates").find(new Document("corporation.vat_number", vat)).into(result);
+
+        AffiliateResponse resp = new AffiliateResponse();
+        resp.response = result;
+
+        return resp;
+
+    }
+    
+    @RequestMapping(method = RequestMethod.GET, value="/user/affiliateByVat/{vat}")
+    public AffiliateResponse getAffiliateUserByVat(@PathVariable String vat) {
+        List<Document> result = new ArrayList<>();
+        mongoClientUser.getDatabase("db-ldl-fr").getCollection("affiliates").find(new Document("corporation.vat_number", vat)).into(result);
 
         AffiliateResponse resp = new AffiliateResponse();
         resp.response = result;
